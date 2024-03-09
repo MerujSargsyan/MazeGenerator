3cfb0e50462992164081e4d997c67d2d0bcac12a
MeruzhanS <meruzhansargsyan48@gmail.com>
2024-03-09 12:08:24 -0800

    working on maze generation

--- maze.py
+++ maze.py
@@ -6,15 +6,27 @@ if len(sys.argv) != 2:
     sys.exit()
 
 size = int(sys.argv[1])
+goal = None
 
 maze_array = [[1]*size for _ in range(size)]
 
+# NEED TO DEFINE WALLS
+def get_adjacent_tiles(starting_idx):
+    pass
+
+def generate_winning_path(starting_point):
+    pass
+    
 def generate_maze():
     start_idx = random.randint(0, size-1)
     end_idx = random.randint(start_idx, size-1)
 
-    maze_array[0][start_idx] = 0
-    maze_array[end_idx][0] = 0
+    maze_array[0][start_idx] = 0.5 # starting point
+    maze_array[end_idx][0] = 0.5 # ending point
+    global goal
+    goal = (end_idx, 0)
 
-    starting_point = maze_array[0][start_idx]
+    starting_point = (0, start_idx)
+    generate_winning_path(starting_point)
+
     
