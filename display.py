0c7488744465d9679375ff0ec7e0c4ba0bbaea69
MeruzhanS <meruzhansargsyan48@gmail.com>
2024-03-09 17:25:22 -0800

    new generating algorithm

--- display.py
+++ display.py
@@ -7,7 +7,7 @@ pygame.init()
 screen = pygame.display.set_mode((square_size * maze.size, square_size * maze.size))
 running = True
 
-maze.generate_maze()
+maze.start_maze_generation()
 
 while running:
     for event in pygame.event.get():
@@ -19,12 +19,25 @@ while running:
     #render
     for row in range(len(maze.maze_array)):
         for col in range(len(maze.maze_array[row])):
-            #black for 0 and white for 1
-            color = 255 * maze.maze_array[row][col]
-            rectColor = (color, color, color)
-            recPos = (row * square_size, col * square_size, square_size, square_size)
-            pygame.draw.rect(screen, rectColor, recPos)
+            cell = maze.maze_array[row][col]
+            walls = cell.walls
 
+            if walls["right"]:
+                pygame.draw.line(screen, "white", 
+                    (cell.x * square_size + square_size, cell.y * square_size), 
+                    (cell.x * square_size + square_size, cell.y * square_size + square_size))
+            if walls["left"]:
+                pygame.draw.line(screen, "white",
+                    (cell.x * square_size, cell.y * square_size), 
+                    (cell.x * square_size, cell.y * square_size + square_size))
+            if walls["top"]:
+                pygame.draw.line(screen, "white",
+                    (cell.x * square_size, cell.y * square_size),
+                    (cell.x * square_size + square_size, cell.y * square_size))
+            if walls["bottom"]:
+                pygame.draw.line(screen, "white",
+                    (cell.x * square_size, cell.y * square_size + 25),
+                    (cell.x * square_size + square_size, cell.y * square_size + square_size))
     pygame.display.flip()
 
 pygame.quit()
--- maze.py
+++ maze.py
@@ -1,32 +1,91 @@
 import sys
-import random 
+import random
 
 if len(sys.argv) != 2:
     print("Please enter the size of the maze")
     sys.exit()
 
 size = int(sys.argv[1])
-goal = None
 
-maze_array = [[1]*size for _ in range(size)]
-
-# NEED TO DEFINE WALLS
-def get_adjacent_tiles(starting_idx):
-    pass
-
-def generate_winning_path(starting_point):
-    pass
+class Cell:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.walls = {'top': True, 'right': True, 'bottom': True,
+            'left': True}
+        self.visited = False
+        self.isStart = False
     
-def generate_maze():
+    def get_neighbor(self):
+        valid_neighbors = []
+        if(self.x+1 < size):
+            valid_neighbors.append(maze_array[self.y][self.x+1])
+        if(self.x-1 >= 0):
+            valid_neighbors.append(maze_array[self.y][self.x-1])
+        if(self.y-1 >= 0):
+            valid_neighbors.append(maze_array[self.y-1][self.x])
+        if(self.y+1 < size):
+            valid_neighbors.append(maze_array[self.y+1][self.x])
+        
+        for neighbor in valid_neighbors:
+            if neighbor.visited == False:
+                return neighbor
+        
+        return None
+
+
+maze_array = [[None] * size for _ in range(size)]
+
+for row in range(len(maze_array)):
+    for col in range(len(maze_array[0])):
+        maze_array[row][col] = Cell(col, row)
+
+print(len(maze_array))
+print(len(maze_array[0]))
+
+
+
+def start_maze_generation():
     start_idx = random.randint(0, size-1)
-    end_idx = random.randint(start_idx, size-1)
 
-    maze_array[0][start_idx] = 0.5 # starting point
-    maze_array[end_idx][0] = 0.5 # ending point
-    global goal
-    goal = (end_idx, 0)
+    start_cell = maze_array[0][start_idx]
+    start_cell.isStart = True
+    generate_maze((start_cell.x, start_cell.y))
 
-    starting_point = (0, start_idx)
-    generate_winning_path(starting_point)
+visited = []
+def generate_maze(starting_pos):
+    x,y = starting_pos
+
+    while len(visited) < size*size:
+        cell = maze_array[x][y]
+        neighbor = cell.get_neighbor()
+        neighbor.visitied = True
+        if neighbor is not None:
+            visited.append(neighbor)
+            connect(cell, neighbor)
+        else:
+            x,y = (visited.pop().x, visited.pop().y)
+    
+
+
+def connect(cell1, cell2):
+    if cell1.x < cell2.x:
+        cell1.walls['right'] = False
+        cell2.walls['left'] = False
+    elif cell1.x > cell2.x:
+        cell1.walls['left'] = False
+        cell2.walls['right'] = False
+    elif cell1.y > cell2.y:
+        cell1.walls['bottom'] = False
+        cell2.walls['top'] = False
+    elif cell1.y < cell2.y:
+        cell1.walls['top'] = False
+        cell2.walls['bottom'] = False
+
+
+
+    
+
+
 
     
